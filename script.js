1376d0e99b5bc87c540db08601fc159d863b82b5
matheustech-1 <mathenrique0905@gmail.com>
2026-02-04 11:30:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-<script>
-  
-  function translatePage(lang) {
-    document.querySelectorAll("[data-i18n]").forEach(el => {const key = el.getAttribute("data-i18n");
-      el.innerText = translations[lang][key] || el.innerText;
-        });
-    localStorege.setItem("lang", lang);
-  }
-
-const saveLang = localStorage.getItem("lang") || "pt";
-document.getElementById("languageSwitcher").value = savedLang;
-translatePage(savedLang);
-
-document.getElementById("languageSwitcher").addEventListener("change", e => translatePage(e.target.value));
-</script>
-
+<
